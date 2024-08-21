9a3f01092006af90b0cfee388d2c294461f07349
bill1118 <bill1118921@gmail.com>
2024-08-21 13:13:10 +0800

    20240821

---
+++ Footer.jsx
@@ -0,0 +1,61 @@
+function Footer() {
+    return (
+        <>
+            <footer>
+                <div>
+                    <h1 class="logo">
+                        <a href="./index.html">
+                            <img src="./images/logo/Union-logo-brown 1.svg" alt=""/>
+                        </a>
+                    </h1>
+                    <nav class="navigation">
+                        <ul class="menu-list">
+                            <li class="menu">
+                                <h2>關西旅遊旅行社有限公司</h2>
+                                <ul class="submenu">
+                                    <li>甲種旅遊業 交觀甲字第90001號</li>
+                                    <li>品保甲北 3000 號</li>
+                                    <li>代表人:關姬</li>
+                                    <li>聯絡人:關東西</li>
+                                </ul>
+                            </li>
+                            <li class="menu">
+                                <h2>聯絡資訊</h2>
+                                <ul class="submenu">
+                                    <li>電話:02-3322-2777</li>
+                                    <li>傳真:02-3322-2778</li>
+                                    <li>信箱:kansaitravel68@kstravel.com.tw</li>
+                                    <li>地址:台北市中正區一段條條大路68號</li>
+                                </ul>
+                            </li>
+                            <li class="menu">
+                                <h2>服務</h2>
+                                <ul class="submenu">
+                                    <li><a href="#">主題行程</a></li>
+                                    <li><a href="#">自由規劃</a></li>
+                                    <li><a href="#">連絡我們</a></li>
+                                    <li><a href="#">會員登入</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu">
+                                <h2>Follow us</h2>
+                                <ul class="submenu-icon">
+                                    <li>
+                                        <a href=""><img src="./images/icon/line.png" alt=""/></a>
+                                    </li>
+                                    <li>
+                                        <a href=""><img src="./images/icon/fb.png" alt=""/></a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="small">
+                    <small>版權所有 © 2024 神火旅遊</small>
+                </div>
+
+            </footer>
+        </>
+    )
+}
