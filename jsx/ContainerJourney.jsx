8bd371c47f43ce66ef25df9de708168f67a7c4a5
bill1118 <bill1118921@gmail.com>
2024-08-23 17:28:39 +0800

    20240823

---
+++ jsx/ContainerJourney.jsx
@@ -0,0 +1,50 @@
+function ContainerJourney() {
+    return <>
+        <section id="container-journey">
+            <div className="box-title">
+                <div className="title-list">
+                    <div className="title">
+                        <h2>天守閣</h2>
+                    </div>
+                    <div className="title">
+                        <h2>道頓堀</h2>
+                    </div>
+                    <div className="title">
+                        <h2>甲賀流</h2>
+                    </div>
+                </div>
+                <div className="content">
+                    <h4>第一天</h4>
+                </div>
+            </div>
+            <div className="box-primary">
+                <figure></figure>
+                <div className="title">
+                    <h1>大阪</h1>
+                </div>
+                <div className="content">
+                    <h3>天守閣</h3>
+                    <p>日本著名的歷史城堡，</p>
+                    <p>為封建權利的象徵之一，</p>
+                    <p>代表擁有者有著強大的武力。</p>
+                    <p>在這裡可以了解</p>
+                    <p>大阪的歷史和文化。</p>
+                    <p>周圍的公園四季皆美，</p>
+                    <p>很適合與朋友散步和拍照。</p>
+                </div>
+            </div>
+            <div className="box-second">
+                <figure className="big"></figure>
+                <figure className="small"></figure>
+                <div className="content">
+                    <h3>道頓堀</h3>
+                    <p>
+                        大阪著名的美食街，<br />
+                        有各種當地特色美食，<br />
+                        如章魚燒、大阪燒和串燒。<br />
+                    </p>
+                </div>
+            </div>
+        </section>
+    </>
+}
