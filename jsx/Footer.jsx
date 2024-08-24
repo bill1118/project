a7036bd9891431113dda5778c96caf3d8e4a0dfd
bill1118 <bill1118921@gmail.com>
2024-08-24 22:12:29 +0800

    20240824

--- jsx/Footer.jsx
+++ jsx/Footer.jsx
@@ -3,43 +3,43 @@ function Footer() {
         <>
             <footer>
                 <div>
-                    <h1 class="logo">
+                    <h1 className="logo">
                         <a href="./index.html">
                             <img src="./images/logo/Union-logo-brown 1.svg" alt=""/>
                         </a>
                     </h1>
-                    <nav class="navigation">
-                        <ul class="menu-list">
-                            <li class="menu">
+                    <nav className="navigation">
+                        <ul className="menu-list">
+                            <li className="menu">
                                 <h2>關西旅遊旅行社有限公司</h2>
-                                <ul class="submenu">
+                                <ul className="submenu">
                                     <li>甲種旅遊業 交觀甲字第90001號</li>
                                     <li>品保甲北 3000 號</li>
                                     <li>代表人:關姬</li>
                                     <li>聯絡人:關東西</li>
                                 </ul>
                             </li>
-                            <li class="menu">
+                            <li className="menu">
                                 <h2>聯絡資訊</h2>
-                                <ul class="submenu">
+                                <ul className="submenu">
                                     <li>電話:02-3322-2777</li>
                                     <li>傳真:02-3322-2778</li>
                                     <li>信箱:kansaitravel68@kstravel.com.tw</li>
                                     <li>地址:台北市中正區一段條條大路68號</li>
                                 </ul>
                             </li>
-                            <li class="menu">
+                            <li className="menu">
                                 <h2>服務</h2>
-                                <ul class="submenu">
+                                <ul className="submenu">
                                     <li><a href="#">主題行程</a></li>
                                     <li><a href="#">自由規劃</a></li>
                                     <li><a href="#">連絡我們</a></li>
                                     <li><a href="#">會員登入</a></li>
                                 </ul>
                             </li>
-                            <li class="menu">
+                            <li className="menu">
                                 <h2>Follow us</h2>
-                                <ul class="submenu-icon">
+                                <ul className="submenu-icon">
                                     <li>
                                         <a href=""><img src="./images/icon/line.png" alt=""/></a>
                                     </li>
@@ -51,7 +51,7 @@ function Footer() {
                         </ul>
                     </nav>
                 </div>
-                <div class="small">
+                <div className="small">
                     <small>版權所有 © 2024 神火旅遊</small>
                 </div>
 
